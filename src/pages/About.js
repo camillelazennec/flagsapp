349211bb2154e71466e538272aca71ef9a509d76
camille <camille.lazennec@ynov.com>
2025-03-14 01:43:28 +0100

    validate project

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>Apropos</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quisquam labore ducimus, illo officia
+                 est omnis id sunt dicta et velit tempora quo neque. Soluta voluptatibus earum quasi, quidem dolorem, 
+                 exercitationem esse non, laborum laudantium dolore labore nostrum placeat porro repellendus. Facere 
+                 sunt modi, asperiores sit libero earum numquam illum maiores consequuntur, natus quae magnam. Ut 
+                 reprehenderit consectetur placeat dolorem nostrum fugiat voluptatum iste aliquid quam atque sunt 
+                 dignissimos aperiam ab pariatur neque doloremque, accusantium eius! Alias asperiores autem amet. Iste, 
+                 commodi sunt eligendi praesentium, hic porro accusantium eum laboriosam, saepe deleniti laborum! 
+                 Veritatis aliquid asperiores laudantium nesciunt? Expedita, enim.
+            </p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quisquam labore ducimus, illo officia
+                 est omnis id sunt dicta et velit tempora quo neque. Soluta voluptatibus earum quasi, quidem dolorem, 
+                 exercitationem esse non, laborum laudantium dolore labore nostrum placeat porro repellendus. Facere 
+                 sunt modi, asperiores sit libero earum numquam illum maiores consequuntur, natus quae magnam. Ut 
+                 reprehenderit consectetur placeat dolorem nostrum fugiat voluptatum iste aliquid quam atque sunt 
+                 dignissimos aperiam ab pariatur neque doloremque, accusantium eius! Alias asperiores autem amet. Iste, 
+                 commodi sunt eligendi praesentium, hic porro accusantium eum laboriosam, saepe deleniti laborum! 
+                 Veritatis aliquid asperiores laudantium nesciunt? Expedita, enim.
+            </p>
+        </div>
+    );
+};
+
+export default About;
